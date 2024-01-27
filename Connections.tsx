3785a41d4adb3aa48eddf0eec658947641f3efca
AlexanderDeFuria <defuria.alexander@gmail.com>
2024-01-27 17:33:34 -0500

    Skeleton is built

---
+++ Connections.tsx
@@ -0,0 +1,13 @@
+export const skeletonMap = new Map([
+    ["nose", ["leftEye", "rightEye"]],
+    ["leftEye", ["leftEar"]],
+    ["rightEye", ["rightEar"]],
+    ["leftShoulder", ["rightShoulder", "leftElbow", "leftHip"]],
+    ["rightShoulder", ["rightElbow", "rightHip"]],
+    ["leftElbow", ["leftWrist"]],
+    ["rightElbow", ["rightWrist"]],
+    ["leftHip", ["leftKnee", "rightHip"]],
+    ["rightHip", ["rightKnee"]],
+    ["leftKnee", ["leftAnkle"]],
+    ["rightKnee", ["rightAnkle"]]
+]);
